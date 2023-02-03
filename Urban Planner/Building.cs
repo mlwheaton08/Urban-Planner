3e6978ebe2c55ab59e4d4e10f9f8fdd0ea15a8a1
Mason Wheaton <mlwheaton06@gmail.com>
2023-02-02 20:41:51 -0600

    Add project files.

---
+++ Urban Planner/Building.cs
@@ -0,0 +1,40 @@
+﻿namespace Urban_Planner;
+
+public class Building
+{
+    private string _designer = "Mason Wheaton";
+    private DateTime _dateConstructed;
+    private string _address;
+    private string _owner;
+
+    public int Stories { get; set; }
+    public double Width { get; set; }
+    public double Depth { get; set; }
+    //public double Volume { get { return Width * Depth * (3 * Stories); } } //essentially makes it readonly, bc can't set it
+    public double Volume => Width * Depth * (3 * Stories); //shorthand way to write the above
+
+    public Building(string address)
+    {
+        _address = address;
+    }
+
+    public void Construct()
+    {
+        _dateConstructed = DateTime.Now;
+    }
+
+    public void Purchase(string name)
+    {
+        _owner = name;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($@"{_address}---------
+Designed by {_designer}
+Constructed on {_dateConstructed}
+Owned by {_owner}
+{Volume} cubic meters of space
+");
+    }
+}
---
+++ Urban Planner/Program.cs
@@ -0,0 +1,20 @@
+﻿using Urban_Planner;
+
+Building building1 = new Building("101 First St") { Width = 500.8, Depth = 200.9, Stories = 3 };
+Building building2 = new Building("102 First St") { Width = 300.8, Depth = 600.9, Stories = 1 };
+Building building3 = new Building("103 First St") { Width = 200.8, Depth = 300.9, Stories = 5 };
+
+building1.Construct();
+building2.Construct();
+building3.Construct();
+
+building1.Purchase("Owen");
+building2.Purchase("Greg");
+building3.Purchase("Sandy");
+
+List<Building> buildings = new List<Building>()
+{
+    building1, building2, building3
+};
+
+buildings.ForEach(building => building.DisplayInfo());
